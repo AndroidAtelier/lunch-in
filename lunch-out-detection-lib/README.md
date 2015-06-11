309dc0770e64d3ef1e9e91e68acab6264185c73b
Mark Scheel <mscheel@itriagehealth.com>
2015-06-11 12:16:46 -0600

    fix locked screen, add readme, add test wifi code

---
+++ lunch-out-detection-lib/README.md
@@ -0,0 +1,4 @@
+Include these permission in your app:
+
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+    <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
--- lunch-out-detection-lib/src/main/java/com/github/androidatelier/lunchin/LunchOutDetectionService.java
+++ lunch-out-detection-lib/src/main/java/com/github/androidatelier/lunchin/LunchOutDetectionService.java
@@ -1,22 +1,36 @@
 package com.github.androidatelier.lunchin;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
 /**
  * Created by A596969 on 6/11/15.
  */
 public class LunchOutDetectionService {
 
-    public LunchOutDetectionService(LunchOutDetectionListener listener) {
+    public LunchOutDetectionService(Context context, LunchOutDetectionListener listener) {
 
-        while(true) {
+        listener.possibleLunchOutDetected();
 
-            try {
-                Thread.sleep(1000 * 60);
-                listener.possibleLunchOutDetected();
-            } catch(InterruptedException e) {
-                //silent fail, bad!
+        BroadcastReceiver myReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                WifiManager wifiManager = (WifiManager) context.getSystemService (Context.WIFI_SERVICE);
+                WifiInfo info = wifiManager.getConnectionInfo ();
+                String ssid = info.getSSID();
+                Log.d("MARK", ssid);
             }
+        };
 
-        }
+        final IntentFilter filters = new IntentFilter();
+        filters.addAction("android.net.wifi.WIFI_STATE_CHANGED");
+        filters.addAction("android.net.wifi.STATE_CHANGE");
+        context.registerReceiver(myReceiver, filters);
     }
 
 }

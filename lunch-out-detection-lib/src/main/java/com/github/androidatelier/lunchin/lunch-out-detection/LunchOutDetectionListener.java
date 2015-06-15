c758cfe003591ad0e261297f4dd06e2b8a83ec09
brenda <brenda@ionu.com>
2015-06-15 16:51:13 -0600

    add the moved files for lunch-out-detection

---
+++ lunch-out-detection-lib/src/main/java/com/github/androidatelier/lunchin/lunch-out-detection/LunchOutDetectionListener.java
@@ -0,0 +1,8 @@
+package com.github.androidatelier.lunch_out_detection;
+
+/**
+ * Created by A596969 on 6/11/15.
+ */
+public interface LunchOutDetectionListener {
+    void possibleLunchOutDetected();
+}

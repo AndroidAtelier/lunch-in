dd5b528be57ac3fbaffe4c433c03c1ae4d896331
jwawrzyn <jwawrzynczak@nationaldecisionsupport.com>
2015-06-16 20:47:52 -0600

    Create API methods and database utility class
    Add 3 api utilities for knowing number of lunch ins, setting a lunch in,
    setting a lunch out
    Create a shell for database utility class

---
+++ database/src/main/java/com/github/androidatelier/database/DatabaseUtility.java
@@ -0,0 +1,7 @@
+package com.github.androidatelier.database;
+
+/**
+ * Created by Jenny on 6/16/2015.
+ */
+public class DatabaseUtility {
+}
--- database/src/main/java/com/github/androidatelier/database/LunchInApi.java
+++ database/src/main/java/com/github/androidatelier/database/LunchInApi.java
@@ -1,7 +1,35 @@
 package com.github.androidatelier.database;
 
+import java.util.Random;
+
 /**
  * Created by Jenny on 6/16/2015.
  */
 public class LunchInApi {
+
+    /**
+     * Get the number of lunch ins the person has used
+     * @return Returns a random number between 0 and 50
+     */
+    public int GetNumberOfLunchIns()
+    {
+        Random generator = new Random();
+        return generator.nextInt(50);
+    }
+
+    /**
+     * Creates another lunch in line in database
+     */
+    public void SetLunchIn()
+    {
+
+    }
+
+    /**
+     * Indicates user went out to lunch instead of eating in
+     */
+    public void SetLunchOut()
+    {
+
+    }
 }

16a816eb98db8fe318fe258e686ec44f2bdf6858
jwawrzyn <jwawrzynczak@nationaldecisionsupport.com>
2015-06-16 21:05:26 -0600

    Starting some database create statements

--- database/src/main/java/com/github/androidatelier/database/DatabaseUtility.java
+++ database/src/main/java/com/github/androidatelier/database/DatabaseUtility.java
@@ -1,7 +1,42 @@
 package com.github.androidatelier.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Jenny on 6/16/2015.
  */
-public class DatabaseUtility {
+public class DatabaseUtility extends SQLiteOpenHelper {
+    public static final String TABLE_LUNCHIN = "LunchIn";
+    public static final String COLUMN_LUNCHIN_ID = "LunchInId";
+    public static final String COLUMN_LUNCHIN_DATE = "LunchInDate";
+    public static final String COLUMN_LUNCHIN_SUCCESS = "Success";
+
+    private static final String DATABASE_NAME = "lunchin.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //database creation
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_LUNCHIN + "(" + COLUMN_LUNCHIN_ID
+            + " integer primary key autoincrement, " + COLUMN_LUNCHIN_DATE
+            + " datetime not null, " + COLUMN_LUNCHIN_SUCCESS
+            + " bit);";
+
+    public DatabaseUtility(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database)
+    {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_LUNCHIN);
+    }
 }

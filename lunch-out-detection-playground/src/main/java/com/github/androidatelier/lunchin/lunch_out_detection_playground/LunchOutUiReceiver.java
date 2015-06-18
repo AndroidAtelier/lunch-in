d330a0ac4d0a111843ad462ed27de94c17463cd2
Mark Scheel <mscheel@itriagehealth.com>
2015-06-17 23:43:46 -0600

    Make playground app work with notifications, brings back context (miscommunication in PR review), adds notification call and related manifest change.

--- lunch-out-detection-playground/src/main/java/com/github/androidatelier/lunchin/lunch_out_detection_playground/LunchOutUiReceiver.java
+++ lunch-out-detection-playground/src/main/java/com/github/androidatelier/lunchin/lunch_out_detection_playground/LunchOutUiReceiver.java
@@ -5,20 +5,24 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.github.androidatelier.lunchin.LunchOutDetectionReceiver;
+import com.github.androidatelier.lunchin.notification.NotificationUtil;
 
 /**
  * Created by Kelly Shuster on 6/17/15.
  */
 public class LunchOutUiReceiver extends LunchOutDetectionReceiver {
 
+    private Context mContext;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        mContext = context;
         super.onReceive(context,intent);
     }
 
     @Override
     public void onPossibleLunchOut() {
         Log.d("KIO", "Inside playground onPossibleLunchOut of LunchOutUiReceiver");
+        NotificationUtil.showLunchOutNotification(mContext, LunchOutUiReceiver.class);
     }
 }

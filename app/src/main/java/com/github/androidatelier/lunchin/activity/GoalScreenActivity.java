897eaf4da6407092b3124f7667694292bfd984e9
Joan Siebenaler <jms80304@gmail.com>
2015-06-18 17:51:21 -0600

    Created screen for the goal's bargraphs.

---
+++ app/src/main/java/com/github/androidatelier/lunchin/activity/GoalScreenActivity.java
@@ -0,0 +1,16 @@
+package com.github.androidatelier.lunchin.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.R;
+import android.R.layout;
+import android.util.Log;
+
+public class GoalScreenActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.github.androidatelier.lunchin.R.layout.activity_goal_screen);
+    }
+}

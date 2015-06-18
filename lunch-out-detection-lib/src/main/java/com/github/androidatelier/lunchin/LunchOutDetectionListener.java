1570f9c7954ff6a951070b9f5894a97a5a91f7c9
KioKrofovitch <kshuster@itriagehealth.com>
2015-06-17 18:46:11 -0600

    Cleanup

--- lunch-out-detection-lib/src/main/java/com/github/androidatelier/lunchin/LunchOutDetectionListener.java
+++ lunch-out-detection-lib/src/main/java/com/github/androidatelier/lunchin/LunchOutDetectionListener.java
@@ -1,7 +1,7 @@
 package com.github.androidatelier.lunchin;
 
 /**
- * Created by A596969 on 6/11/15.
+ * Created by Mark S on 6/11/15.
  */
 public interface LunchOutDetectionListener {
     void possibleLunchOutDetected();

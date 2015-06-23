3b96e8293ff7ed3588525cbc4269b2f40af24e64
Chiu-Ki Chan <chiuki@gmail.com>
2015-06-22 22:02:02 -0600

    Use getNumberOfLunchInsThisMonth in StatsFragment

--- app/src/main/java/com/github/androidatelier/lunchin/fragment/StatsFragment.java
+++ app/src/main/java/com/github/androidatelier/lunchin/fragment/StatsFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.androidatelier.database.LunchInApi;
 import com.github.androidatelier.lunchin.R;
 import com.github.androidatelier.lunchin.model.SettingsAccess;
 
@@ -17,28 +18,41 @@ import java.text.NumberFormat;
 /**
  * Created by brenda on 6/17/15.
  */
-public class StatsFragment extends Fragment{
-
+public class StatsFragment extends Fragment {
     private SettingsAccess mSettingsAccess;
+    private LunchInApi mLunchInApi;
+
+    private TextView mLunchCostInMinutes;
+    private TextView mLunchInsThisMonth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.fragment_stats,container,false);
-        TextView tv2 = (TextView) v.findViewById(R.id.fragment_stats_tv2);
+        mLunchCostInMinutes = (TextView) v.findViewById(R.id.fragment_stats_tv2);
+        mLunchInsThisMonth = (TextView) v.findViewById(R.id.fragment_stats_tv3);
 
         mSettingsAccess = new SettingsAccess(getActivity());
+        mLunchInApi = new LunchInApi(getActivity());
+
+        update();
+
+        return v;
+    }
+
+    public void update() {
         double salary = mSettingsAccess.getGrossAnnualSalary();
         double lunch_cost = mSettingsAccess.getAverageLunchCost();
         double hourly_wage = salary/(40.0 * 52.0); //40 hours in a week and 52 weeks in a year
 
         String strHoursLunch = String.format("You would have to work %.1f minutes " +
-                "to buy lunch that costs %s with an annual salary of %s",
+                        "to buy lunch that costs %s with an annual salary of %s",
                 lunch_cost/hourly_wage * 60,
                 NumberFormat.getCurrencyInstance().format(lunch_cost),
                 NumberFormat.getCurrencyInstance().format(salary));
 
-        tv2.setText(strHoursLunch);
-        return v;
-    }
+        mLunchCostInMinutes.setText(strHoursLunch);
 
+        mLunchInsThisMonth.setText(
+                getString(R.string.lunch_ins_this_month, mLunchInApi.getNumberOfLunchInsThisMonth()));
+    }
 }

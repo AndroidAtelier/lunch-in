ae035550099e66db4579dda9b94e51b211e42a6b
Chiu-Ki Chan <chiuki@gmail.com>
2015-06-23 22:00:48 -0600

    Show progress toward goal on MyGoalFragment

--- app/src/main/java/com/github/androidatelier/lunchin/fragment/MyGoalFragment.java
+++ app/src/main/java/com/github/androidatelier/lunchin/fragment/MyGoalFragment.java
@@ -1,5 +1,6 @@
 package com.github.androidatelier.lunchin.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,35 +16,44 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.androidatelier.database.LunchInApi;
 import com.github.androidatelier.lunchin.R;
 import com.github.androidatelier.lunchin.model.SettingsAccess;
 import com.github.androidatelier.lunchin.util.Formatter;
 
-public class MyGoalFragment extends Fragment{
-    private SettingsAccess  mSettingsAccess;
-
+public class MyGoalFragment extends Fragment implements Updateable {
     private static final String TAG = "MyGoalFragment";
+
+    private SettingsAccess mSettingsAccess;
+    private LunchInApi mLunchInApi;
+
+    private TextView mGoalProgress;
     ImageView imageView;
 
-   @Override
+    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_my_goal, container, false);
-
-        imageView = (ImageView) v.findViewById(R.id.dollarView);
+        View v =inflater.inflate(R.layout.fragment_my_goal,container,false);
+        mGoalProgress = (TextView) v.findViewById(R.id.fragment_my_goal_progress_gained);
 
         mSettingsAccess = new SettingsAccess(getActivity());
+        mLunchInApi = new LunchInApi(getActivity());
+
         initializeViews(v);
         initializeGoalAchievedAnimation(v);
+
+        update();
 
         return v;
     }
 
     private void initializeViews(View v) {
-        TextView goalName = (TextView)v.findViewById(R.id.fragment_my_goal_name);
+        TextView goalName = (TextView) v.findViewById(R.id.fragment_my_goal_name);
         goalName.setText(mSettingsAccess.getSavingsGoalName());
 
-        TextView goalValue = (TextView)v.findViewById(R.id.fragment_my_goal_value);
+        TextView goalValue = (TextView) v.findViewById(R.id.fragment_my_goal_value);
         goalValue.setText(Formatter.formatIntToCurrencyUSD(mSettingsAccess.getSavingsGoalValue()));
+
+        imageView = (ImageView) v.findViewById(R.id.dollarView);
     }
 
     private void initializeGoalAchievedAnimation(View v) {
@@ -92,6 +102,21 @@ public class MyGoalFragment extends Fragment{
         });
     }
 
+    @Override
+    public void update() {
+        Activity activity = getActivity();
+        if (activity == null || activity.isFinishing()) {
+            return;
+        }
+        double averageLunchCost = mSettingsAccess.getAverageLunchCost();
+        int goal = mSettingsAccess.getSavingsGoalValue();
+        int numLunchIns = mLunchInApi.getNumberOfLunchIns();
+
+        mGoalProgress.setText(getString(R.string.goal_progress,
+                Formatter.formatDoubleToCurrencyUSD(numLunchIns * averageLunchCost),
+                Formatter.formatIntToCurrencyUSD(goal)));
+    }
+
     // animation
     public void zoom(View view) {
         //ImageView image = (ImageView)view.findViewById(R.id.dollarView);
@@ -120,7 +145,7 @@ public class MyGoalFragment extends Fragment{
 
     private void scaleAnimateSelectImageView(ImageView imageView) {
         Log.v(TAG, "animateSelectButton");
-        ScaleAnimation scaleAnimation = new ScaleAnimation(0.1f, 1.2f, 0.1f, 1.2f, Animation.RELATIVE_TO_SELF, (float)0.5, Animation.RELATIVE_TO_SELF, (float)0.5);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0.1f, 1.2f, 0.1f, 1.2f, Animation.RELATIVE_TO_SELF, (float) 0.5, Animation.RELATIVE_TO_SELF, (float) 0.5);
         scaleAnimation.setFillAfter(true);
         scaleAnimation.setFillEnabled(true);
         scaleAnimation.setDuration(800);

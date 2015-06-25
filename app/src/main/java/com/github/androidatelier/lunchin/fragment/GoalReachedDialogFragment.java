d8620b2e86a3f15f6159559cd18a8bcec8fc2456
Chiu-Ki Chan <chiuki@gmail.com>
2015-06-25 12:27:22 -0600

    Animate dollar view on GoalReachedDialogFragment

--- app/src/main/java/com/github/androidatelier/lunchin/fragment/GoalReachedDialogFragment.java
+++ app/src/main/java/com/github/androidatelier/lunchin/fragment/GoalReachedDialogFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.androidatelier.lunchin.R;
@@ -29,12 +32,19 @@ public class GoalReachedDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_goal_reached, null);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.dollarView);
+        Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.dollar);
+        imageView.startAnimation(animation);
+
         final TextView nameView = (TextView) view.findViewById(R.id.dialog_goal_name);
         String goalName = getArguments().getString(Constants.KEY_GOAL_NAME);
         nameView.setText(goalName);
+
         final TextView costView = (TextView) view.findViewById(R.id.dialog_goal_cost);
         int goalCost = getArguments().getInt(Constants.KEY_GOAL_COST);
         costView.setText(Formatter.formatIntToCurrencyUSD(goalCost));
+
         builder.setView(view);
 
         view.setOnClickListener(new View.OnClickListener() {
